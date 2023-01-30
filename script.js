1b7698a0ac873295dc5eb667f263860408f7df81
ActAngel <dazeg001@hotmail.com>
2023-01-31 00:47:20 +0100

    Test

--- script.js
+++ script.js
@@ -1,63 +1,12 @@
-// array of hacks
-const hacks = [
-    { name: "hack1", description: "Description of hack 1", execute: function() { /* code to execute hack 1 */ } },
-    { name: "hack2", description: "Description of hack 2", execute: function() { /* code to execute hack 2 */ } },
-    // add more hacks here
-  ];
-  
-  // function to display command prompt
-  function displayPrompt() {
-    let input = prompt("Enter a command:");
-    handleInput(input);
-    }
+function startHack() {
+  const targets = document.getElementById("targets");
+  const selectedTarget = targets.options[targets.selectedIndex].value;
 
-  
-  // function to handle player's input
-  function handleInput(input) {
-    // check if input matches any of the hack names
-    for (let i = 0; i < hacks.length; i++) {
-      if (input === hacks[i].name) {
-        // execute corresponding hack function
-        let result = hacks[i].execute();
-        // display result in output div
-        document.getElementById("output").innerHTML += result + "<br>";
-        return;
-      }
-    }
-    // if no match is found, display error message
-    document.getElementById("output").innerHTML += "Invalid command.<br>";
+  if (selectedTarget === "target1") {
+    document.getElementById("result").innerHTML = "Hack misslyckades.";
+  } else if (selectedTarget === "target2") {
+    document.getElementById("result").innerHTML = "Hack lyckades!";
+  } else if (selectedTarget === "target3") {
+    document.getElementById("result").innerHTML = "Målet är skyddat med en stark säkerhet, hack misslyckades.";
   }
-  
-  document.getElementById("start-button").addEventListener("click", function() {
-    startGame();
-  });
-  
-  document.getElementById("start-button").addEventListener("click", function() {
-    document.getElementById("menu").style.display = "block";
-  });
-
-  const { exec } = require("child_process");
-
-const commands = [
-  { name: "ls", description: "List directory contents", execute: function() {
-    exec("ls", (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-    });
-  } },
-  // add more commands here
-];
-
-
-
-
-  
-  
-  
+}
